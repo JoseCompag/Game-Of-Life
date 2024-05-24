eaf895bba77c7008005977ed40ad35ca2673e385
JoseCompag <joseic2000@gmail.com>
2024-05-24 00:54:46 -0300

    Added tests for MockOutput class

--- src/main/java/output/MockOutput.java
+++ src/main/java/output/MockOutput.java
@@ -1,4 +1,36 @@
 package output;
 
-public class MockOutput {
+import board.Board;
+import game.Game;
+
+public class MockOutput implements Observer {
+
+  private boolean invoked = false;
+  private Game game;
+  private Board board;
+  private Integer generation;
+
+  public MockOutput(Game game) {
+    this.game = game;
+    game.registerObserver(this);
+  }
+
+  @Override
+  public void update(Board board, Integer generation) {
+    this.board = board;
+    this.generation = generation;
+    invoked = true;
+  }
+
+  public boolean isInvoked() {
+    return invoked;
+  }
+
+  public Board getBoard() {
+    return board;
+  }
+
+  public Integer getGeneration() {
+    return generation;
+  }
 }
--- src/test/java/output/OutputTests.java
+++ src/test/java/output/OutputTests.java
@@ -1,4 +1,39 @@
 package output;
 
+import board.Board;
+import board.RectangularBoard;
+import game.Game;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import rule.Rule;
+
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+
 public class OutputTests {
+
+  static Stream<Arguments> outputs() {
+    return Stream.of(
+      //
+      arguments()
+    );
+  }
+
+  @Test
+  public void output() {
+    Board board = new RectangularBoard(8, 8, new ArrayList<Rule>());
+    Game game = new Game(board);
+    MockOutput mockOutput = new MockOutput(game);
+    game.start(5);
+    assertThat(mockOutput.isInvoked()).isTrue();
+    assertThat(mockOutput.getGeneration()).isEqualTo(5);
+
+  }
+
 }
