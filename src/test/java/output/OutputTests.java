f1d42d6fd141fcce805f18de1c0f078c795cd0dc
JoseCompag <joseic2000@gmail.com>
2024-06-08 19:55:38 -0300

    File generation tests

--- src/test/java/output/OutputTests.java
+++ src/test/java/output/OutputTests.java
@@ -2,11 +2,17 @@ package output;
 
 import board.Board;
 import board.RectangularBoard;
+import cell.CellFactoryTraditionalGame;
+import game.Game;
 import gamecontroller.GameController;
 import org.junit.jupiter.api.*;
+import rule.BirthRule;
+import rule.DeathRule;
 import rule.Rule;
+import rule.SurviveRule;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -21,6 +27,35 @@ public class OutputTests {
     game.start(5);
     assertThat(mockOutput.isInvoked()).isTrue();
     assertThat(mockOutput.getGeneration()).isEqualTo(5);
+  }
+
+  @Test
+  public void output2() {
+    String configInitial =
+      "□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ \n" +
+        "□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ \n" +
+        "□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ \n" +
+        "□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ \n" +
+        "□ □ □ □ □ □ □ □ □ ■ ■ ■ □ □ □ □ □ □ □ □ \n" +
+        "□ □ □ □ □ □ □ □ ■ □ □ □ ■ □ □ □ □ □ □ □ \n" +
+        "□ □ □ □ □ □ □ ■ □ □ □ □ □ ■ □ □ □ □ □ □ \n" +
+        "□ □ □ □ □ □ □ ■ □ □ □ □ □ ■ □ □ □ □ □ □ \n" +
+        "□ □ □ □ □ □ □ ■ □ □ □ □ □ ■ □ □ □ □ □ □ \n" +
+        "□ □ □ □ □ □ □ □ ■ □ □ □ ■ □ □ □ □ □ □ □ \n" +
+        "□ □ □ □ □ □ □ □ □ ■ ■ ■ □ □ □ □ □ □ □ □ \n" +
+        "□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ \n" +
+        "□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ \n" +
+        "□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ \n" +
+        "□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ "+"\n";
+    Board board = new RectangularBoard(15, 20, configInitial, new CellFactoryTraditionalGame());
+    List<Rule> rules = new ArrayList<>();
+    rules.add(new SurviveRule(List.of(2,3)));
+    rules.add(new BirthRule(List.of(3)));
+    rules.add(new DeathRule());
+    board.setRules(rules);
+    GameController game = new GameController(board);
+    FileOutput fileOutput = new FileOutput(game);
+    game.start(5);
   }
 
 }
