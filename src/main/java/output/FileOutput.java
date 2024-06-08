cd66c8215cbc887bdd3b24002fac0bc9e584850d
JoseCompag <joseic2000@gmail.com>
2024-06-08 19:47:30 -0300

    FileOutput Implementation

---
+++ src/main/java/output/FileOutput.java
@@ -0,0 +1,32 @@
+package output;
+
+
+import board.Board;
+import gamecontroller.GameController;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileOutput implements Observer {
+
+  private GameController gameController;
+  private String path = "../tp2-GameOfLife/files";
+
+  public FileOutput(GameController game) {
+    this.gameController = game;
+    game.registerObserver(this);
+  }
+
+  @Override
+  public void update(Board board, Integer generation) {
+    try {
+      FileWriter fw = new FileWriter(new File(path,"generation"+generation+".txt"));
+      fw.write(board.toString());
+      fw.close();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+
+  }
+}
