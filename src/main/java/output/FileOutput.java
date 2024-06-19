201b2bdeae44d47d11b9517648685b50f2fce918
JoseCompag <joseic2000@gmail.com>
2024-06-18 23:34:42 -0300

    New properties

--- src/main/java/output/FileOutput.java
+++ src/main/java/output/FileOutput.java
@@ -7,26 +7,32 @@ import gamecontroller.*;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class FileOutput implements Output {
 
   private GameController gameController;
-  private String path = "../tp2-GameOfLife/files";
+  private String path = "files";
 
   public FileOutput(GameController game) {
     this.gameController = game;
     game.registerObserver(this);
   }
 
-  @Override
   public void update(Board board, Integer generation) {
     try {
-      FileWriter fw = new FileWriter(new File(path,"generation"+generation+".txt"));
+      Path directoryPath = Paths.get(path);
+      if (!Files.exists(directoryPath)) {
+        Files.createDirectories(directoryPath);
+      }
+
+      FileWriter fw = new FileWriter(new File(directoryPath.toFile(), "generation" + generation + ".txt"));
       fw.write(board.toString());
       fw.close();
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
-
   }
 }
